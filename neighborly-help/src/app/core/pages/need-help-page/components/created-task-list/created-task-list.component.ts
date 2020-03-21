56a3b488eee56de248a74c4f4b453cf2dd255142
xmichasx <xmichas@gmail.com>
2020-03-22 00:52:27 +0100

    modals

--- neighborly-help/src/app/core/pages/need-help-page/components/created-task-list/created-task-list.component.ts
+++ neighborly-help/src/app/core/pages/need-help-page/components/created-task-list/created-task-list.component.ts
@@ -5,6 +5,10 @@ import {
   translateBoolean,
   translateTypeToPolish,
 } from '../../../../shared/shared/utils';
+import { ActivityType } from '../../../../../model/activity-type';
+import { TaskState } from '../../../../../model/task/task-state';
+import { TaskApi } from '../../../../../api/task-api';
+import { NzNotificationService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'nh-created-task-list',
@@ -12,6 +16,16 @@ import {
   styleUrls: ['./created-task-list.component.scss'],
 })
 export class CreatedTaskListComponent implements OnInit {
+  task: Task;
+  isVisible = false;
+  activityType = ActivityType;
+  taskState = TaskState;
+  okTextButton: string;
+  cancelTextButton: string;
+  constructor(
+    private service: TaskApi,
+    private notification: NzNotificationService
+  ) {}
   ngOnInit() {}
   paginatedData: PaginationList;
 
@@ -56,12 +70,68 @@ export class CreatedTaskListComponent implements OnInit {
     },
   ];
 
+  showModal($event: Task) {
+    this.task = $event;
+    if (this.task && this.task.state === TaskState.ACCEPTED) {
+      this.okTextButton = 'Zakończono pomyślnie';
+      this.cancelTextButton = 'Zakończono niepowozeniem';
+    } else {
+      this.okTextButton = 'Usuń';
+      this.cancelTextButton = 'Anuluj';
+    }
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    if (this.task && this.task.state === TaskState.ACCEPTED) {
+      this.service.finishDone(this.task.id).subscribe(() => {
+        this.isVisible = false;
+        this.notification.success(
+          'Zakończono pomyślnie',
+          'Zgłoszenie zakończone pomyślnie'
+        );
+      });
+    } else {
+      this.service.delete(this.task.id).subscribe(() => {
+        this.isVisible = false;
+        this.notification.success(
+          'Usunięto pomyślnie',
+          'Zgłoszenie zostało usunięte'
+        );
+      });
+    }
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+  handleCancelDelete(): void {
+    if (this.task && this.task.state === TaskState.ACCEPTED) {
+      this.service.finishFail(this.task.id).subscribe(() => {
+        this.isVisible = false;
+        this.notification.success(
+          'Zakończono niepowodzeniem',
+          'Zgłoszenie zakończone niepowodzeniem'
+        );
+      });
+    } else {
+      this.service.cancel(this.task.id).subscribe(() => {
+        this.isVisible = false;
+        this.notification.success(
+          'Pomyślnie anulowano',
+          'Zgłoszenie zostało anulowane'
+        );
+      });
+    }
+  }
+
   private mapTaskToListRow(task: Task) {
     return {
       id: task.id,
       type: translateTypeToPolish(task.type),
       address: task.address.city + ' ' + task.address.street,
       epidemicDanger: translateBoolean(task.epidemicDanger),
+      task: task,
     };
   }
 }
