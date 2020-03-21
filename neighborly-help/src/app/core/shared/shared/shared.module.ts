bfcaa6c6b27bf1430aaf32eaee0c40e3a21f5d9d
xmichasx <xmichas@gmail.com>
2020-03-21 21:50:23 +0100

    Task details

--- neighborly-help/src/app/core/shared/shared/shared.module.ts
+++ neighborly-help/src/app/core/shared/shared/shared.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedListComponent } from './components/shared-list';
 import { AgGridModule } from 'ag-grid-angular';
+import {
+  NzDescriptionsModule,
+  NzGridModule,
+  NzModalModule,
+} from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [SharedListComponent],
   exports: [SharedListComponent],
-  imports: [CommonModule, AgGridModule],
+  imports: [
+    CommonModule,
+    AgGridModule,
+    NzModalModule,
+    NzGridModule,
+    NzDescriptionsModule,
+  ],
 })
 export class SharedModule {}
