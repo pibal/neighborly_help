14bfbe766e00aaa26ceed74a4c671c66cd28d886
nevaan <pawel.losek@outlook.com>
2020-03-21 19:26:46 +0100

    Update retype-password control validation on password change in register form

--- neighborly-help/src/app/authentication/components/register-form/register-form.component.ts
+++ neighborly-help/src/app/authentication/components/register-form/register-form.component.ts
@@ -1,18 +1,22 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Credentials } from 'src/app/firebase/auth';
 import { samePasswordValidator } from './util';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'nh-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss'],
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnDestroy {
   formGroup: FormGroup;
 
   @Output() register = new EventEmitter<Credentials>();
   @Output() cancel = new EventEmitter();
+
+  private unsub$ = new Subject();
 
   constructor(private fb: FormBuilder) {
     this.formGroup = this.fb.group({
@@ -27,6 +31,23 @@ export class RegisterFormComponent {
         ],
       ],
     });
+
+    this.formGroup
+      .get('password')
+      .valueChanges.pipe(
+        tap(_ =>
+          this.formGroup
+            .get('retype-password')
+            .updateValueAndValidity({ emitEvent: false })
+        ),
+        takeUntil(this.unsub$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.unsub$.next();
+    this.unsub$.complete();
   }
 
   onRegister() {
