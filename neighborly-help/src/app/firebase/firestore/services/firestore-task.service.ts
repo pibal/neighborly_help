74c9422b94db952fcfc6f3f6747149e1e09511dc
ukasz <lukas.matuszyk@gmail.com>
2020-03-22 00:24:12 +0100

    Add states changing between states

--- neighborly-help/src/app/firebase/firestore/services/firestore-task.service.ts
+++ neighborly-help/src/app/firebase/firestore/services/firestore-task.service.ts
@@ -4,7 +4,7 @@ import { Observable, from, of } from 'rxjs';
 import { Task } from 'src/app/model/task/task';
 import { FirestoreCollectionsNames } from '../enum/firestore-collections-names';
 import { FirebaseTask } from '../firebase-model/firebase-task';
-import { first, map, switchAll, switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { mapTaskFromFirebase } from '../utils/firebase-object-mappers';
 import { TaskCreationDTO } from 'src/app/model/task/task-creation-dto';
 import { TaskApi } from 'src/app/api/task-api';
@@ -15,6 +15,10 @@ import { ChangedPath } from 'ag-grid-community';
 
 @Injectable()
 export class FirestoreTaskService extends TaskApi {
+  // TODO: get acceptor ID from auth context
+  private creatorID: string = '5sALe2N1MC33P7ldmDnv';
+  private executorID: string = 'jslXfh1FYBbKr7PxOGvoj5zmkqt2';
+
   constructor(
     private database: AngularFirestore,
     private userAPiService: AppUserApi
@@ -42,8 +46,10 @@ export class FirestoreTaskService extends TaskApi {
 
   public create(creationDTO: TaskCreationDTO): Observable<string> {
     //TODO: get uder ID from auth context
-    const userID = '5sALe2N1MC33P7ldmDnv';
-    const firebaseTask = createFirebaseTaskFromCreationDTO(creationDTO, userID);
+    const firebaseTask = createFirebaseTaskFromCreationDTO(
+      creationDTO,
+      this.creatorID
+    );
     const taskCollection = this.database.collection<FirebaseTask>(
       FirestoreCollectionsNames.TASK
     );
@@ -51,19 +57,17 @@ export class FirestoreTaskService extends TaskApi {
   }
 
   public accept(id: string): Observable<any> {
-    // TODO: get acceptor ID from auth context
-    const userID = '5sALe2N1MC33P7ldmDnv';
     const taskCollection = this.database.collection<FirebaseTask>(
       FirestoreCollectionsNames.TASK
     );
 
-    return this.userAPiService.get(userID).pipe(
+    return this.userAPiService.get(this.executorID).pipe(
       switchMap(appUser => {
         return from(
           taskCollection.doc(id).update({
             state: TaskState.ACCEPTED,
             executorPhoneNumber: appUser.phoneNumber,
-            executorID: userID,
+            executorID: appUser.uid,
           })
         );
       })
@@ -71,18 +75,59 @@ export class FirestoreTaskService extends TaskApi {
   }
 
   public cancel(id: string): Observable<any> {
-    throw new Error('Method not implemented.');
+    const taskCollection = this.database.collection<FirebaseTask>(
+      FirestoreCollectionsNames.TASK
+    );
+    return from(
+      taskCollection.doc(id).update({
+        state: TaskState.CANCELED,
+      })
+    );
   }
   public delete(id: string): Observable<any> {
-    throw new Error('Method not implemented.');
+    const taskCollection = this.database.collection<FirebaseTask>(
+      FirestoreCollectionsNames.TASK
+    );
+    return from(
+      taskCollection.doc(id).update({
+        state: TaskState.DELETED,
+      })
+    );
   }
   public resign(id: string): Observable<any> {
-    throw new Error('Method not implemented.');
+    const taskCollection = this.database.collection<FirebaseTask>(
+      FirestoreCollectionsNames.TASK
+    );
+    return from(
+      taskCollection.doc(id).update({
+        state: TaskState.RESIGNED,
+        executorID: '',
+        executorPhoneNumber: '',
+      })
+    );
   }
   public finishDone(id: string): Observable<any> {
-    throw new Error('Method not implemented.');
+    const taskCollection = this.database.collection<FirebaseTask>(
+      FirestoreCollectionsNames.TASK
+    );
+    return from(
+      taskCollection.doc(id).update({
+        state: TaskState.DONE,
+        rate: {
+          rate: 3,
+          comment: 'TEST COMMENT',
+        },
+      })
+    );
   }
   public finishFail(id: string): Observable<any> {
-    throw new Error('Method not implemented.');
+    const taskCollection = this.database.collection<FirebaseTask>(
+      FirestoreCollectionsNames.TASK
+    );
+    return from(
+      taskCollection.doc(id).update({
+        state: TaskState.FAILED,
+      })
+    );
   }
 }
