2cbaa8f01b77eec764cb91af0bdf1c03e2d5be5c
Piotr Bala <piotrbala@yahoo.pl>
2020-03-21 19:06:07 +0100

    Task Creation  - Added form for task creations

--- neighborly-help/src/app/core/pages/need-help-page/containers/add-task/add-task.component.ts
+++ neighborly-help/src/app/core/pages/need-help-page/containers/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivityType } from '../../../../../model/activity-type';
+import { TaskApi } from '../../../../../api/task-api';
 
 @Component({
   selector: 'nh-add-task',
@@ -6,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent implements OnInit {
-  constructor() {}
+  activityType = ActivityType;
 
-  ngOnInit() {}
+  taskForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private service: TaskApi) {}
+
+  ngOnInit(): void {
+    this.taskForm = this.fb.group({
+      type: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      details: [null],
+      creatorPhoneNumber: [null, [Validators.required]],
+      epidemicDanger: [true],
+      localization: [null],
+      address: [null, [Validators.required]],
+    });
+  }
+
+  submitForm(): void {
+    for (const i in this.taskForm.controls) {
+      this.taskForm.controls[i].markAsDirty();
+      this.taskForm.controls[i].updateValueAndValidity();
+    }
+    if (this.taskForm.valid) {
+      this.service.create({
+        ...this.taskForm.value,
+        // TODO: Replace with true values
+        localization: {
+          longitude: 20,
+          latitude: 50,
+        },
+      });
+    }
+  }
 }
--- neighborly-help/src/app/core/pages/need-help-page/need-help-page.module.ts
+++ neighborly-help/src/app/core/pages/need-help-page/need-help-page.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AddTaskComponent } from './containers/add-task/add-task.component';
 import { NeedHelpPageRoutingModule } from './need-help-page-routing.module';
 import { SubmittedTasksComponent } from './containers/submitted-tasks/submitted-tasks.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FirebaseModule } from '../../../firebase/firebase.module';
+import { SharedModule } from '../../shared/shared/shared.module';
 
 @NgModule({
   declarations: [AddTaskComponent, SubmittedTasksComponent],
-  imports: [CommonModule, NeedHelpPageRoutingModule],
+  imports: [
+    SharedModule,
+    FirebaseModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgZorroAntdModule,
+    CommonModule,
+    NeedHelpPageRoutingModule,
+  ],
 })
 export class NeedHelpPageModule {}
